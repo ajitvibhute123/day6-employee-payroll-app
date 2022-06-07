c3aae5b4bacbe693b18b677044ab776f7a0c6d61
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-06-08 02:01:59 +0530

    Added: Validation to Name Field so the REST call can be validated

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployePayrollAppApplication.java
@@ -14,6 +14,8 @@ public class EmployePayrollAppApplication {
 		ApplicationContext context = SpringApplication.run(EmployePayrollAppApplication.class, args);
 		log.info("Employee Payroll App Started in {} Environment",
 				context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll DB User is {} ",
+				context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
